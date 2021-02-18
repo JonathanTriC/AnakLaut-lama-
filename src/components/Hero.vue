c9ccc8f25de5b0c26efbb21501e75732531f0d80
JonathanTriC <JonathanTriC@users.noreply.github.com>
2021-02-18 12:03:50 +0700

    update hero landing page

---
+++ src/components/Hero.vue
@@ -0,0 +1,31 @@
+<template>
+	<!-- <div class="flex w-full h-full">
+		<img :src="image" alt="gambar hero" />
+	</div> -->
+	<div class="hero">
+		<img :src="image" alt="gambar hero" />
+		<div class="text-white ">
+			<div class="p-24">
+				<h1 class="mb-7 text-5xl font-bold">
+					Laut Kami, <br />
+					Untuk Kami.
+				</h1>
+
+				<button class="btn btn-primary">Jelajahi</button>
+			</div>
+		</div>
+	</div>
+</template>
+
+<script>
+import heroImg from "../assets/images/design/hero-img.png";
+
+export default {
+	data: function() {
+		return {
+			image: heroImg,
+		};
+	},
+	name: "Hero",
+};
+</script>
--- src/main.js
+++ src/main.js
@@ -1,4 +1,21 @@
-import { createApp } from 'vue'
-import App from './App.vue'
+import { createApp } from "vue";
+import App from "./App.vue";
 
-createApp(App).mount('#app')
+import "./assets/styles/index.css";
+
+export const eventBus = createApp(App);
+
+createApp(App).mount("#app");
+
+// import Vue from "vue";
+// import App from "./App.vue";
+
+// import "./assets/styles/index.css";
+
+// // Vue.config.productionTip = false;
+
+// export const eventBus = new Vue();
+
+// new Vue({
+// 	render: (h) => h(App),
+// }).$mount("#app");
